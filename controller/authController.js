e24b7051f4ad34221e670c3dbb3f19f276d85e99
ALUNO SENAI PETROLINA <aluno.senaipetrolina@SISTEMAFIEPE.LOCAL>
2026-02-04 08:54:45 -0300

    feat: adiciona hash de senha no cadastro

--- controller/authController.js
+++ controller/authController.js
@@ -1,13 +1,17 @@
 const { PrismaClient } = require("@prisma/client")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const prisma = new PrismaClient()
 
 exports.criarUsuario = async (reg,res) =>{
 
     const {email,senha} = req.body
+    
+    const hashSenha = await bcrypt.hash(senha,8)
     //Aplicativo do prisma
-    const usuario = await prisma.usuario.create({data: {email,senha}})
-    res.status(201).json(usuario)
+    const usuario = await prisma.usuario.create({data: {email,senha:hashSenha}})
+    res.status(201).json(id: usuario.id, email: usuario.email)
 }
 
 exports.login = async (req,res)=>{
